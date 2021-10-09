031550132dd93b6c0c694333f6143233ccde5d99
JoseEPina <jose.edpina@gmail.com>
2021-10-09 15:38:28 -0600

    Reused original Index file. Added NPM package uid2 and implemented Post and Delete methods under notesRoutes.js. Current WORKING version

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,14 +1,48 @@
 const router = require('express').Router();
-const { renderNoteList } = require('../../public/assets/js/renderNotes.js');
 const { notes } = require('../../db/db.json');
+//const { renderNoteList, renderActiveNote,  } = requi
+const uid = require('uid2'); // npm package to generate an ID value
+
+const fs = require('fs'); // Needed here to use writeFile
+const path = require('path'); // module utility to for working with file and dir paths
 
 router.get('/notes', (req, res) => {
-   let results = notes;
    console.log(req.query);
-   if (req.query) {
-      results = renderNoteList(req.query, results);
-   }
-   res.json(results);
+   res.json(notes);
+   // let results = notes; // Probably no longer needed
+   // if (req.query) {
+   //    results = renderActiveNote(req.query, results);
+   // }
+});
+
+router.post('/notes', (req, res) => {
+   // req.body is where our incoming content will be
+   req.body.id = uid(10); // to create the id of the note using uid package
+   const newNote = req.body;
+   notes.push(newNote); // push new note data to JS array
+
+   // Synchronously write (doesn't require a callback function)
+   // using writeFile or writeFileAsync results in error
+   fs.writeFileSync(
+      path.join(__dirname, '../../db/db.json'), // path to write to
+      // Save JS array data as JSON. Other two arguments used, will keep data formatted.
+      JSON.stringify({ notes }, null, 2)
+   );
+   res.json(notes); // ?? to send data as JSON response? But where?
+});
+
+router.delete('/notes/:id', (req, res) => {
+   // notes path needed /:id in order for it to work
+   const id = req.params.id; // from newly generated data
+   const noteIndex = notes.findIndex((element) => element.id === id);
+   notes.splice(noteIndex, 1); // splice method to remove selected index in our array
+
+   // Synchronously write (in this case "remove")
+   fs.writeFileSync(
+      path.join(__dirname, '../../db/db.json'), //
+      JSON.stringify({ notes }, null, 2) // keeping data formatted just as before.
+   );
+   res.json(notes); // ?? to send data as JSON response? But where?
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const { notes } = require('./db/db.json');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
- const fs = require('fs');
- const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
+
 // parse incoming JSON data
 app.use(express.json());
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
 // express.static() method. The way it works is that we provide a file path to a location in our application
 // (in this case, the public folder) and instruct the server to make these files static resources.
 // This means that all of our front-end code can now be accessed without having a specific server endpoint created for it!
